deac9f361c5da5077b15641515b4c4a24e5ba565
Ioana Ifrim <ioana.ifrim@gmail.com>
2020-05-22 14:52:12 +0200

    working with multiple input tensors

--- inference/include/DLInference.h
+++ inference/include/DLInference.h
@@ -14,31 +14,32 @@ public:
     void SetModelGraph(const std::string &aModelGraph);
     void SetModelRestore(const std::string &aModelRestore);
     void SetInputNode(const std::string &anInputNode);
+    void SetExtraInputNode(const std::string &anExtraInputNode);
     void SetLabelNode(const std::string &aLabelNode);
     void SetOutputNode(const std::string &anOutputNode);
     void SetInputShape(const  std::vector<int64_t> &anInputShape);
+    void SetExtraInputShape(const  std::vector<int64_t> &anExtraInputShape);
     void SetLabelShape(const std::vector<int64_t> &aLabelShape);
     void SetEnergyValue(const int &anEnergyValue);
     void SetInputVecNumber(const int &anInputVecNumber);
+    void SetExtraInputVecNumber(const int &anExtraInputVecNumber);
 
 private:
 
-    std::string modelType;
     int energyValue;
+    std::string modelType;
     std::string modelGraph;
     std::string modelRestore;
     std::string inputNode;
+    std::string extraInputNode = "";
     std::string labelNode;
-    std::string xiNode;
     std::string outputNode;
 
-    int inputSize;
-    int labelSize;
-    int inputVecNumber;
     std::vector<int64_t> inputShape;
+    std::vector<int64_t> extraInputShape;
     std::vector<int64_t> labelShape;
-
-    std::string outFileName;
+    int inputVecNumber;
+    int extraInputVecNumber;
 
 };
 
